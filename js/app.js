6f4a1f50edd4904ee8e6d31318a13d074801eba5
José Luis Escobar Pérez <65322045+luisescobar09@users.noreply.github.com>
2023-03-07 02:28:28 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/PWA-CV/serviceWorker.js')
+        navigator.serviceWorker.register('/PWA-CV/sw/sw.js')
             .then(reg => console.log('Service Worker: Registered'))
             .catch(err => console.log(`Service Worker: Error: ${err}`))
     })
